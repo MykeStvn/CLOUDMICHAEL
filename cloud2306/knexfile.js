cb6e036f4cbac717657a81fdd65778907524ddff
MykeStvn <110644468+MykeStvn@users.noreply.github.com>
2024-07-16 08:21:33 -0500

    Cambio de nombre bdd

--- cloud2306/knexfile.js
+++ cloud2306/knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
       host: '127.0.0.1',
       user: 'root',
       password: '',
-      database:'cloud23'
+      database:'pruebamichael'
     },
     migrations:{
       directory: './migrations'
